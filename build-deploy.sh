365ede5d4ee2708aab19be770c9e6ed8b3306395
xantrix <x4ntrix@gmail.com>
2019-06-10 10:34:21 +0200

    tune build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,6 +1,6 @@
-#gcloud builds submit --tag gcr.io/cr-test-wordpress/wp:v1
-#gcloud beta run deploy --image gcr.io/cr-test-wordpress/wp:v1 --add-cloudsql-instances mysql
-#gcloud beta run services delete wp
+gcloud builds submit --tag gcr.io/cr-test-wordpress/wp:v1
+gcloud beta run deploy --image gcr.io/cr-test-wordpress/wp:v1 --add-cloudsql-instances mysql
+gcloud beta run services delete wp
 gcloud beta run deploy wp --image gcr.io/cr-test-wordpress/wp:v1 \
   --add-cloudsql-instances mysql \
   --update-env-vars DB_HOST='127.0.0.1',DB_NAME=wordpress,DB_USER=wordpress,DB_PASSWORD=wordpress,CLOUDSQL_INSTANCE='cr-test-wordpress:us-central1:mysql'
